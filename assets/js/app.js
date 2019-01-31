5ca7bb3bbbf5e6b7bd1047821dccf294d9f2bacd
Alireza <aryadesignco@gmail.com>
2019-01-31 13:28:52 +0330

    Slider Added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,29 @@
 $(".bag").click(function() {
     $(".bag .cart").slideToggle();
+});
+$(".slider .next").click(function() {
+    var slideLen = $(".slider .slide").length;
+    var active = $(".slide.active").data("slide");
+    if (active < slideLen) {
+        var next = active + 1;
+        $(".slide.active").removeClass("active");
+        $(".slide[data-slide="+next+"]").addClass("active");
+    } else if (active == slideLen) {
+        var next = 1;
+        $(".slide.active").removeClass("active");
+        $(".slide[data-slide="+next+"]").addClass("active");
+    }
+})
+$(".slider .prev").click(function() {
+    var slideLen = $(".slider .slide").length;
+    var active = $(".slide.active").data("slide");
+    if (active < slideLen && active >= 1) {
+        var prev = active - 1;
+        $(".slide.active").removeClass("active");
+        $(".slide[data-slide="+prev+"]").addClass("active");
+    } else if (active == slideLen) {
+        var prev = slideLen;
+        $(".slide.active").removeClass("active");
+        $(".slide[data-slide="+next+"]").addClass("active");
+    }
 })
